d58590564d25281fa943e1a3705ef5a5147b4ce7
CreamyMilk <creamym254@gmail.com>
2020-08-22 20:56:04 +0300

    FIxed login stuff

--- routes/auth/a_tenats.js
+++ routes/auth/a_tenats.js
@@ -14,34 +14,37 @@ let refreshStore =  []
     //res.json({mesage:"Allowed header is POST"})
 
 router.post('/login',(req,res)=>{
-    const email = req.body.email
-    const password = req.body.password
+    const email = req.body.email || ""
+    const password = req.body.password || "" 
     let hash = new Buffer(password).toString('base64');
-    pool.query('SELECT r_email,r_password,rid FROM tbl_add_rent WHERE r_email=? LIMIT 1',[email], (err,rows) => {
-        if(err) throw err;
-        let user = rows[0]
-        if(hash === user["r_password"]){
-            const refreshtoken = fun.generateRefreshToken({user})
-            refreshStore.push(refreshtoken)
-            res.json({
-                access_token : fun.generateAccessToken({user}),
-                refresh_token: refreshtoken
-            })
+    pool.query('SELECT r_email,r_password,rid,r_name,r_contact,branch_id,image FROM tbl_add_rent WHERE r_email=?LIMIT 1',[email], (err,rows) => {
+        if(rows.length === 0){
+            res.json({status:"Wrong email or password"})
         }else{
-            res.json(user)
+            let user = rows[0]
+            if(hash === user["r_password"]){
+                const refreshtoken = fun.generateRefreshToken({user})
+                refreshStore.push(refreshtoken)
+                res.json({
+                    access_token : fun.generateAccessToken({user}),
+                    refresh_token: refreshtoken
+                })
+            }else{
+                res.json({status:"Wrong email or password"})
+            }
         }
     });
 })
 //refresh token
 router.post('/token', (req, res) => {
-    const refreshToken = req.body.token
+    const refreshToken = req.body.token 
     if (refreshToken == null) return res.sendStatus(401)
     if (!refreshStore.includes(refreshToken)) return res.sendStatus(403)
     jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET,(err,newTokenUser)=>{
         if (err) return res.sendStatus(403) //Watu wajucange
         //parse user object for important info
         const access_token = fun.generateAccessToken({
-            rid: newTokenUser.user.rid
+            rid: newTokenUser.user
         })
         res.json({access_token})
     })
--- routes/tenants.js
+++ routes/tenants.js
@@ -1,7 +1,33 @@
+const fun = require("../jwtauthfunc/authtokens")
 const router = require("express").Router()
+const pool = require("../database/database")
 
-router.get('/',(req,res)=>{
-    res.json({me:"ok"})
+router.get('/notice',fun.authToken,(req,res)=>{
+    let branch_id = req.user.user.branch_id 
+    pool.query("SELECT * FROM tbl_notice_board WHERE branch_id=?",[branch_id],(err,rows)=>{
+        if(err) throw err;
+        console.log()
+        res.json(rows)
+    })
+})
+
+router.get('/complains',fun.authToken,(req,res)=>{
+    let branch_id = req.user.user.branch_id 
+    let user_id  = req.user.user.rid 
+    pool.query("SELECT *,m.month_name from tbl_add_complain c inner join tbl_add_month_setup m on m.m_id = c.c_month where c.branch_id =? and c.c_userid =? order by complain_id DESC LIMIT 5",[branch_id,user_id],(err,rows)=>{
+        if(err) throw err;
+        console.log()
+        res.json(rows)
+    })
+})
+router.get('/all',fun.authToken,(req,res)=>{
+    let branch_id = req.user.user.branch_id 
+    let user_id  = req.user.user.rid 
+    pool.query("",(err,rows)=>{
+        if(err) throw err;
+        console.log()
+        res.json(rows)
+    })
 })
 
 module.exports = router
