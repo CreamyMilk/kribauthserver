2ece53fccb40449accf19a246e7b950d40d1d5eb
CreamyMilk <creamym254@gmail.com>
2020-08-24 23:05:47 +0300

    Units basic😊

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ app.use(express.json())
 const ownerAuthRoute = require('./routes/auth/a_owners')
 const tenantAuthRoute = require('./routes/auth/a_tenats')
 const tenantRoute = require('./routes/tenant')
-
+const ownerRoute = require('./routes/owner')
 
 //Handlers
 app.use("/api/v1/auth/owner",ownerAuthRoute)
 app.use("/api/v1/auth/tenant",tenantAuthRoute)
 app.use("/api/v1/tenant",tenantRoute)
+app.use("/api/v1/owner",ownerRoute)
 
 app.use((req,res,next)=>{
 	res.json({status:"healthy"})
--- routes/owner.js
+++ routes/owner.js
@@ -3,8 +3,12 @@ const router = require("express").Router()
 const pool = require("../database/database")
 
 router.get('/units',fun.authToken,(req,res)=>{
-    let units = {}
-    res.json(units)
+    pool.query("SELECT * FROM tbl_add_owner_unit_relation",(err,rows)=>{
+        if(err) throw err;
+        res.json(rows)
+    })
+
+    //res.json(units)
 })
 
 
